12dbab159b5be79a0767d07d03b3e85b6442640e
Jamie <???>
2016-05-07 09:15:26 -0700

    Removed dirty hacks, division operation
    Precision of double floating point is the limiting factor.  Dirty tricks
    buy only a few extra iterations.

--- src/prog.c
+++ src/prog.c
@@ -3,72 +3,43 @@
 #include <stdint.h>
 #include <assert.h>
 #include <math.h>
+#include <stdint.h>
 
-#define T long long int
+#define long_t int64_t
+#define long_t_max (INT64_MAX)
 
-T fib_iter(int n) {
+long_t fib_iter(int n) {
   assert(n >= 0);
 
   if (n == 0) return 0;
   if (n == 1) return 1;
 
-  T prevPrev = 0;
-  T prev = 1;
-  T result = 0;
+  long_t prevPrev = 0;
+  long_t prev = 1;
+  long_t result = 0;
 
-  for (int i = 2; i <= n; i++)
-  {
-      result = prev + prevPrev;
-      prevPrev = prev;
-      prev = result;
+  for (int i = 2; i <= n; i++) {
+    result = prev + prevPrev;
+    prevPrev = prev;
+    prev = result;
   }
 
   return result;
 }
 
 
-
-
-
-double fib_g(int n) {
-
-
-
+long_t fib_g(int n) {
+  // Constants
   const double phi_d = (1.0 + sqrt(5.0))/2.0;
+  const double bottom_inv = 1.0/(2.0*phi_d - 1.0);
 
   const double top_left = pow(phi_d, n);
   const double top_right = pow(1.0 - phi_d, n);
 
-  const double bottom = 2.0*phi_d - 1.0;
+  const double result = (top_left - top_right)*bottom_inv;
 
-  const double result = (top_left - top_right)/bottom;
-
-
-
-
-
-  const double bias = 2.3461048565937045e-15;
-
-  const double offset = result*bias;
-  
-  const double offset_result = result - offset;
-
-
-
-
-  const double rounded = floor(offset_result + 0.1);
-
-  printf("\n");
-  printf("Result %lf\n", result);
-  printf("Offset %lf\n", offset);
-  printf("Offset result %lf\n", offset_result);
-
-  return rounded;
-
+  return result;
 }
-
-
-
 
 
 void test(void)
@@ -76,20 +47,22 @@ void test(void)
   const int max = 20000;
 
   for (int kk = 0; kk <= max; kk++) {
-    const T result_iter = fib_iter(kk);
-    const double result_g = fib_g(kk);
+    const long_t result_iter = fib_iter(kk);
+    const long_t result_g = fib_g(kk);
 
-    printf("%d: %lld, %lf (%lld)\n", kk, result_iter, result_g, (T) result_g);
+    // How far to overflow?
+    const double frac = ((double) result_iter)/long_t_max;
 
-    assert(result_iter == (T) result_g);
+    printf("%d: %lld, %lld (%.10lf)\n", kk, result_iter, result_g, frac);
+
+    assert(result_iter == result_g);
   }
 }
 
 
 int main()
 {
-  assert(sizeof(T) == sizeof(int64_t));
-  assert(sizeof(T) == sizeof(double));
+  assert(sizeof(long_t) == sizeof(double));
 
   test();
 
